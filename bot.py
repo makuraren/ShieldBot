28eed130f1353a6029bfd0d98ba9465307e788b8
Matt.M <wildcatdud@gmail.com>
2020-09-19 04:14:27 -0400

    Revamped Bot Format
    WE GOT COGS BABY

--- bot.py
+++ bot.py
@@ -4,7 +4,9 @@ from discord.ext import commands # For discord
 import json # For interacting with jason
 from pathlib import Path # For paths
 import platform # For stats
-import logging # For logging
+import logging # For loggin
+import datetime
+import os
 
 cwd = Path(__file__).parents[0]
 cwd = str(cwd)
@@ -16,37 +18,37 @@ client = commands.Bot(command_prefix='.', case_insensitive=True, owner_id = 3002
 client.config_token = secret_file['token']
 logging.basicConfig(level=logging.INFO)
 
-client.version = '0.1.1'
+client.version = '0.1.2'
 
 client.blacklisted_users = []
+
+client.colors = {
+  'WHITE': 0xFFFFFF,
+  'AQUA': 0x1ABC9C,
+  'GREEN': 0x2ECC71,
+  'BLUE': 0x3498DB,
+  'PURPLE': 0x9B59B6,
+  'LUMINOUS_VIVID_PINK': 0xE91E63,
+  'GOLD': 0xF1C40F,
+  'ORANGE': 0xE67E22,
+  'RED': 0xE74C3C,
+  'NAVY': 0x34495E,
+  'DARK_AQUA': 0x11806A,
+  'DARK_GREEN': 0x1F8B4C,
+  'DARK_BLUE': 0x206694,
+  'DARK_PURPLE': 0x71368A,
+  'DARK_VIVID_PINK': 0xAD1457,
+  'DARK_GOLD': 0xC27C0E,
+  'DARK_ORANGE': 0xA84300,
+  'DARK_RED': 0x992D22,
+  'DARK_NAVY': 0x2C3E50
+}
+client.color_list = [c for c in client.colors.values()]
 
 @client.event
 async def on_ready():
     print(f"-----\nLogged in as: {client.user.name} <@{client.user.id}>\n-----\nMy current prefix is: .\n-----")
-    data = read_json("blacklist")
-    client.blacklist_users = data["blacklistedUsers"]
     await client.change_presence(activity=discord.Game(name=f"\"Don'\t hurt me\" -{client.user.name}")) # This changes the bots 'activity'
-
-@client.event
-async def on_command_error(ctx, error):
-    #Ignore these errors
-    ignored = (commands.CommandNotFound, commands.UserInputError)
-    if isinstance(error, ignored):
-        return
-
-    #Begin error handling
-    if isinstance(error, commands.CommandOnCooldown):
-        m, s = divmod(error.retry_after, 60)
-        h, m = divmod(m, 60)
-        if int(h) == 0 & int(m) == 0:
-            await ctx.send(f'You must wait {int(s)} seconds to use this command!')
-        elif int(h) == 0 & int(m) != 0:
-            await ctx.send(f'You must wait {int(m)} minutes and {int(s)} seconds to use this command!')
-        else:
-            await ctx.send(f'You must wait {int(h)} hours, {int(m)} minutes and {int(s)} seconds to use this command!')
-    elif isinstance(error, commands.CheckFailure):
-        await ctx.send("Hey! You lack permission to use this command.")
-    raise error
 
 @client.event
 async def on_message(message):
@@ -55,7 +57,7 @@ async def on_message(message):
         return
 
     #Blacklist system
-    if message.author.id in client.blacklist_users:
+    if message.author.id in client.blacklisted_users:
         return
 
     if message.content.lower().startswith("help"):
@@ -63,89 +65,10 @@ async def on_message(message):
 
     await client.process_commands(message)
 
-@client.command()
-@commands.is_owner()
-async def blacklist(ctx, user: discord.Member):
-    """
-    A command that blacklists a user {owner only}
-    """
-    if ctx.message.author.id == user.id:
-        await ctx.send("Hey, you cannot blacklist yourself!")
-        return
-
-    client.blacklisted_users.append(user.id)
-    data = read_json("blacklist")
-    data["blacklistedUsers"].append(user.id)
-    write_json(data, "blacklist")
-    await ctx.send(f"Hey, I have blacklisted {user.name} for you.")
-
-@client.command()
-@commands.is_owner()
-async def unblacklist(ctx, user: discord.Member):
-    """
-    A command that blacklists a user {owner only}
-    """
-    client.blacklisted_users.remove(user.id)
-    data = read_json("blacklist")
-    data["blacklistedUsers"].remove(user.id)
-    write_json(data, "blacklist")
-    await ctx.send(f"Hey, I have unblacklisted {user.name} for you.")
-
-@client.command(name='hi', aliases=['hello'])
-async def _hi(ctx):
-    """
-    A simple command which says hi to the author.
-    """
-    await ctx.send(f"Hi {ctx.author.mention}!")
-
-@client.command()
-async def stats(ctx):
-    """
-    A useful command that displays bot statistics.
-    """
-    pythonVersion = platform.python_version()
-    dpyVersion = discord.__version__
-    serverCount = len(client.guilds)
-    memberCount = len(set(client.get_all_members()))
-
-    embed = discord.Embed(title = f'{client.user.name} Stats', description = '\uFEFF', colour = ctx.author.colour, timestamp = ctx.message.created_at)
-
-    embed.add_field(name = 'Bot Version:', value = client.version)
-    embed.add_field(name = 'Python Version:', value = pythonVersion)
-    embed.add_field(name = 'Discord.py Version', value = dpyVersion)
-    embed.add_field(name = 'Total Guilds:', value = serverCount)
-    embed.add_field(name = 'Total Users:', value = memberCount)
-    embed.add_field(name = 'Bot\'s Father:', value = "<@300251178107928576>")
-
-    embed.set_footer(text = f"{client.user.name} | {client.user.id}")
-    embed.set_author(name = client.user.name, icon_url = client.user.avatar_url)
-
-    await ctx.send(embed = embed)
-@client.command(aliases = ['disconnect', 'close', 'stopbot'])
-@commands.is_owner()
-async def logout(ctx):
-    """
-    If the user running the command owns the bot then this will disconnect the bot from discord.
-    """
-    await ctx.send(f"Hey {ctx.author.mention}, I am now logging out :wave:")
-    await client.logout()
-
-@client.command()
-async def echo(ctx, *, message=None):
-    """
-    A simple command that repeats the users input back to them.
-    """
-    message = message or "Please provide the message to be repeated."
-    await ctx.message.delete()
-    await ctx.send(message)
-
-def read_json(filename):
-    with open(f"{cwd}/client_config/{filename}.json", "r") as file:
-        data = json.load(file)
-    return data
-
-def write_json(data, filename):
-    with open(f"{cwd}/client_config/{filename}.json", "w") as file:
-        json.dump(data, file, indent=4)
-
-client.run(client.config_token)
+if __name__ == '__main__':
+    # When running this file, if it is the 'main' filename
+    # I.E. its not being imported from another python file run this
+    for file in os.listdir(cwd+"/cogs"):
+        if file.endswith(".py") and not file.startswith("_"):
+            client.load_extension(f"cogs.{file[:-3]}")
+    client.run(client.config_token)
---
+++ cogs/events.py
@@ -0,0 +1,66 @@
+import discord
+from discord.ext import commands
+import random
+import datetime
+
+# In cogs we make our own class
+# for d.py which subclasses commands.Cog
+
+class Events(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Events Cog had been loaded\n-----")
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = discord.utils.get(member.guild.text_channels, name = 'general')
+        if channel:
+            embed = discord.Embed(description = 'Welcome to our guild!', color = random.choice(self.client.color_list))
+            embed.set_thumbnail(url = member.avatar_url)
+            embed.set_author(name = member.name, icon_url = member.avatar_url)
+            embed.set_footer(text = member.guild, icon_url = member.guild.icon_url)
+            embed.timestamp = datetime.datetime.utcnow()
+
+            await channel.send(embed = embed)
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        channel = discord.utils.get(member.guild.text_channels, name = 'general')
+        if channel:
+            embed = discord.Embed(description = 'Goodbye from all of us..', color = random.choice(self.client.color_list))
+            embed.set_thumbnail(url = member.avatar_url)
+            embed.set_author(name = member.name, icon_url = member.avatar_url)
+            embed.set_footer(text = member.guild, icon_url = member.guild.icon_url)
+            embed.timestamp = datetime.datetime.utcnow()
+
+            await channel.send(embed = embed)
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        #Ignore these errors
+        ignored = (commands.CommandNotFound, commands.UserInputError)
+        if isinstance(error, ignored):
+            return
+
+        #Begin error handling
+        if isinstance(error, commands.CommandOnCooldown):
+            m, s = divmod(error.retry_after, 60)
+            h, m = divmod(m, 60)
+            if int(h) == 0 & int(m) == 0:
+                await ctx.send(f'You must wait {int(s)} seconds to use this command!')
+            elif int(h) == 0 & int(m) != 0:
+                await ctx.send(f'You must wait {int(m)} minutes and {int(s)} seconds to use this command!')
+            else:
+                await ctx.send(f'You must wait {int(h)} hours, {int(m)} minutes and {int(s)} seconds to use this command!')
+        elif isinstance(error, commands.CheckFailure):
+            await ctx.send("Hey! You lack permission to use this command.")
+        raise error
+
+
+
+def setup(client):
+    client.add_cog(Events(client))
